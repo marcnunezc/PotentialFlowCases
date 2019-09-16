90721a40bfea06afed0bce54db8dfe0504c6f599
Inigo Lopez <inigolcanalejo@gmail.com>
2019-09-16 17:14:33 +0200

    adding comments

--- 3d_validation_cases/RunSingleCase3D/generate_finite_wing_surface.py
+++ 3d_validation_cases/RunSingleCase3D/generate_finite_wing_surface.py
@@ -4,6 +4,10 @@
 ### This file is generated automatically by SALOME v8.4.0 with dump python functionality
 ###
 
+# Indicate parameters:
+AOA = 5.0
+Wing_span = 4.0
+
 import sys
 import salome
 
@@ -12,7 +16,8 @@ theStudy = salome.myStudy
 
 import salome_notebook
 notebook = salome_notebook.NoteBook(theStudy)
-sys.path.insert( 0, r'/home/inigo/simulations/wing/01_only_wing_for_embedded')
+import os
+script_path = os.path.dirname(os.path.realpath(__file__))
 
 ###
 ### GEOM component
@@ -26,30 +31,59 @@ import SALOMEDS
 
 geompy = geomBuilder.New(theStudy)
 
+# Create origin and axis
 O = geompy.MakeVertex(0, 0, 0)
 OX = geompy.MakeVectorDXDYDZ(1, 0, 0)
 OY = geompy.MakeVectorDXDYDZ(0, 1, 0)
 OZ = geompy.MakeVectorDXDYDZ(0, 0, 1)
+
+# Create naca 0012
 Curve_UpperSurface_LE = geompy.MakeCurveParametric("t - 0.5", "0.0", "0.6*(0.2969*sqrt(t) - 0.1260*t - 0.3516*t**2 + 0.2843*t**3 - 0.1036*t**4)", 0, 0.5, 999, GEOM.Interpolation, True)
 Curve_UpperSurface_TE = geompy.MakeCurveParametric("t - 0.5", "0.0", "0.6*(0.2969*sqrt(t) - 0.1260*t - 0.3516*t**2 + 0.2843*t**3 - 0.1036*t**4)", 0.5, 1, 999, GEOM.Interpolation, True)
 Curve_LowerSurface_TE = geompy.MakeCurveParametric("t - 0.5", "0.0", "-0.6*(0.2969*sqrt(t) - 0.1260*t - 0.3516*t**2 + 0.2843*t**3 - 0.1036*t**4)", 0.5, 1, 999, GEOM.Interpolation, True)
 Curve_LowerSurface_LE = geompy.MakeCurveParametric("t - 0.5", "0.0", "-0.6*(0.2969*sqrt(t) - 0.1260*t - 0.3516*t**2 + 0.2843*t**3 - 0.1036*t**4)", 0, 0.5, 999, GEOM.Interpolation, True)
+
+# Put edges together
 Wire_Airfoil = geompy.MakeWire([Curve_UpperSurface_LE, Curve_UpperSurface_TE, Curve_LowerSurface_TE, Curve_LowerSurface_LE], 1e-07)
-geompy.Rotate(Wire_Airfoil, OY, 5*math.pi/180.0)
-Extrusion_1 = geompy.MakePrismVecH(Wire_Airfoil, OY, 4)
+
+# Rotate around center and angle AOA
+geompy.Rotate(Wire_Airfoil, OY, AOA*math.pi/180.0)
+
+# Extrude upper and lower surface
+Extrusion_1 = geompy.MakePrismVecH(Wire_Airfoil, OY, Wing_span)
+
+# Translate wire to make the face later on
 Translation_Airfoil = geompy.MakeTranslationVectorDistance(Wire_Airfoil, OY, 4)
+
+# Making lateral faces
 Face_Airfoil_Left = geompy.MakeFaceWires([Wire_Airfoil], 1)
 Face_Airfoil_Right = geompy.MakeFaceWires([Translation_Airfoil], 1)
+
+# Fuse surfaces
 Fuse_Wing = geompy.MakeFuseList([Extrusion_1, Face_Airfoil_Left, Face_Airfoil_Right], True, True)
+
+# Explode edges
 [Edge_LE,Edge_Left_LowerLE,Edge_Left_UpperLE,Edge_Right_LowerLE,Edge_Right_UpperLE,Edge_Middle_Lower,Edge_Middle_Upper,Edge_Left_LowerTE,Edge_Left_UpperTE,Edge_Right_LowerTE,Edge_Right_UpperTE,Edge_TE] = geompy.ExtractShapes(Fuse_Wing, geompy.ShapeType["EDGE"], True)
+
+# Make groups for meshing later on
+
+# Lateral leading edge airfoils
 Auto_group_for_Sub_mesh_AirfoilLE = geompy.CreateGroup(Fuse_Wing, geompy.ShapeType["EDGE"])
 geompy.UnionList(Auto_group_for_Sub_mesh_AirfoilLE, [Edge_Left_LowerLE, Edge_Left_UpperLE, Edge_Right_LowerLE, Edge_Right_UpperLE])
+
+# Lateral trailing edge airfoils
 Auto_group_for_Sub_mesh_AirfoilTE = geompy.CreateGroup(Fuse_Wing, geompy.ShapeType["EDGE"])
 geompy.UnionList(Auto_group_for_Sub_mesh_AirfoilTE, [Edge_Left_LowerTE, Edge_Left_UpperTE, Edge_Right_LowerTE, Edge_Right_UpperTE])
+
+# Leading and trailing edge
 Auto_group_for_Sub_mesh_LETE = geompy.CreateGroup(Fuse_Wing, geompy.ShapeType["EDGE"])
 geompy.UnionList(Auto_group_for_Sub_mesh_LETE, [Edge_LE, Edge_TE])
+
+# Middle edges
 Auto_group_for_Sub_mesh_Middle = geompy.CreateGroup(Fuse_Wing, geompy.ShapeType["EDGE"])
 geompy.UnionList(Auto_group_for_Sub_mesh_Middle, [Edge_Middle_Lower, Edge_Middle_Upper])
+
+# Adding to study to see in GUI
 geompy.addToStudy( O, 'O' )
 geompy.addToStudy( OX, 'OX' )
 geompy.addToStudy( OY, 'OY' )
@@ -88,9 +122,10 @@ geompy.addToStudyInFather( Fuse_Wing, Auto_group_for_Sub_mesh_Middle, 'Auto_grou
 import  SMESH, SALOMEDS
 from salome.smesh import smeshBuilder
 
+# 2D mesh parameters
 smesh = smeshBuilder.New(theStudy)
-Mesh_1 = smesh.Mesh(Fuse_Wing)
-NETGEN_2D = Mesh_1.Triangle(algo=smeshBuilder.NETGEN_2D)
+Mesh_Wing = smesh.Mesh(Fuse_Wing)
+NETGEN_2D = Mesh_Wing.Triangle(algo=smeshBuilder.NETGEN_2D)
 NETGEN_2D_Parameters_1 = NETGEN_2D.Parameters()
 NETGEN_2D_Parameters_1.SetMaxSize( 0.05 )
 NETGEN_2D_Parameters_1.SetOptimize( 1 )
@@ -98,23 +133,37 @@ NETGEN_2D_Parameters_1.SetFineness( 2 )
 NETGEN_2D_Parameters_1.SetMinSize( 0.01 )
 NETGEN_2D_Parameters_1.SetUseSurfaceCurvature( 1 )
 NETGEN_2D_Parameters_1.SetQuadAllowed( 0 )
-NETGEN_2D_Parameters_1.SetSecondOrder( 143 )
-NETGEN_2D_Parameters_1.SetFuseEdges( 128 )
-Regular_1D = Mesh_1.Segment(geom=Auto_group_for_Sub_mesh_AirfoilLE)
+NETGEN_2D_Parameters_1.SetSecondOrder( 0 )
+NETGEN_2D_Parameters_1.SetFuseEdges( 1 )
+
+# Lateral leading edge airfoils
+Regular_1D = Mesh_Wing.Segment(geom=Auto_group_for_Sub_mesh_AirfoilLE)
 Start_and_End_Length_LE = Regular_1D.StartEndLength(0.01,0.05,[])
 Start_and_End_Length_LE.SetObjectEntry( 'Fuse_Wing' )
-Regular_1D_1 = Mesh_1.Segment(geom=Auto_group_for_Sub_mesh_AirfoilTE)
+
+# Lateral trailing edge airfoils
+Regular_1D_1 = Mesh_Wing.Segment(geom=Auto_group_for_Sub_mesh_AirfoilTE)
 Start_and_End_Length_TE = Regular_1D_1.StartEndLength(0.05,0.01,[])
 Start_and_End_Length_TE.SetObjectEntry( 'Fuse_Wing' )
-Regular_1D_2 = Mesh_1.Segment(geom=Auto_group_for_Sub_mesh_LETE)
+
+# Leading and trailing edge
+Regular_1D_2 = Mesh_Wing.Segment(geom=Auto_group_for_Sub_mesh_LETE)
 Local_Length_LETE = Regular_1D_2.LocalLength(0.01,None,1e-07)
-Regular_1D_3 = Mesh_1.Segment(geom=Auto_group_for_Sub_mesh_Middle)
+
+# Middle edges
+Regular_1D_3 = Mesh_Wing.Segment(geom=Auto_group_for_Sub_mesh_Middle)
 Local_Length_Middle = Regular_1D_3.LocalLength(0.05,None,1e-07)
-isDone = Mesh_1.Compute()
+isDone = Mesh_Wing.Compute()
 Sub_mesh_AirfoilLE = Regular_1D.GetSubMesh()
 Sub_mesh_AirfoilTE = Regular_1D_1.GetSubMesh()
 Sub_mesh_LETE = Regular_1D_2.GetSubMesh()
 Sub_mesh_Middle = Regular_1D_3.GetSubMesh()
+
+NumberOfNodes = Mesh_Wing.NbNodes()
+NumberOfElements = Mesh_Wing.NbTriangles()
+print(' Information about volume mesh:')
+print(' Number of nodes       :', NumberOfNodes)
+print(' Number of elements    :', NumberOfElements)
 
 
 ## Set names of Mesh objects
@@ -125,7 +174,7 @@ smesh.SetName(NETGEN_2D_Parameters_1, 'NETGEN 2D Parameters_1')
 smesh.SetName(Local_Length_Middle, 'Local Length_Middle')
 smesh.SetName(Start_and_End_Length_TE, 'Start and End Length_TE')
 smesh.SetName(Local_Length_LETE, 'Local Length_LETE')
-smesh.SetName(Mesh_1.GetMesh(), 'Mesh_1')
+smesh.SetName(Mesh_Wing.GetMesh(), 'Mesh_Wing')
 smesh.SetName(Sub_mesh_LETE, 'Sub-mesh_LETE')
 smesh.SetName(Sub_mesh_AirfoilTE, 'Sub-mesh_AirfoilTE')
 smesh.SetName(Sub_mesh_AirfoilLE, 'Sub-mesh_AirfoilLE')
